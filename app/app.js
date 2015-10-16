31443be337831ad26225c77655c5782707616507
Anish Shah <anishshah101@gmail.com>
2015-10-16 15:40:24 +0530

    Modified app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+/*
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
@@ -9,4 +10,15 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}]); */
+
+var app = angular.module('FoursquareApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+app.config(function ($routeProvider) {
+ 
+    $routeProvider.when("/explore", {
+        controller: "placesExplorerController",
+        templateUrl: "/app/views/placesresults.html"
+    });
+    $routeProvider.otherwise({ redirectTo: "/explore" });
+ 
+});
